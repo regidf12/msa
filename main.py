cf8310c2d9c1f9e7af86abb9adf0599db6241942
Tilted322 <TiltedFear@gmail.com>
2022-03-14 22:06:20 +0300

    doing

--- main.py
+++ main.py
@@ -1,18 +1,52 @@
-import speech_recognition
-
-if __name__ == "__main__":
-
-    # инициализация инструментов распознавания и ввода речи
-    recognizer = speech_recognition.Recognizer()
-    microphone = speech_recognition.Microphone()
-
-    while True:
-        # старт записи речи с последующим выводом распознанной речи 
-        voice_input = record_and_recognize_audio()
-        print(voice_input)
+import pyttsx3  # синтез речи
+from vosk import Model, KaldiRecognizer  # оффлайн-распознавание
+import speech_recognition  # распознавание пользовательской речи
+import wave  # создание и чтение аудиофайлов формата wav
+import json  # работа с json-файлами и json-строками
+import os  # работа с файловой системой
 
 
-def record_and_recognize_audio(*args: tuple):
+class VoiceAssistant:
+    """
+    Настройка голосового ассистента
+    """
+    name = ""
+    sex = ""
+    speech_language = ""
+    recognition_lenguage = ""
+
+
+def setup_assistant_voice():
+    """
+    Установка голоса по умолчанию (индекс может меняться в 
+    зависимости от настроек операционной системы)
+    """
+    voices = ttsEngine.getProperty("voices")
+
+    if assistant.speech_language == "en":
+        assistant.recognition_language = "en-US"
+        if assistant.sex == "female":
+            # Microsoft Zira Desktop - English (United States)
+            ttsEngine.setProperty("voice", voices[1].id)
+        else:
+            # Microsoft David Desktop - English (United States)
+            ttsEngine.setProperty("voice", voices[2].id)
+    else:
+        assistant.recognition_language = "ru-RU"
+        # Microsoft Irina Desktop - Russian
+        ttsEngine.setProperty("voice", voices[0].id)
+
+
+def play_voice_assistant_speech(text_to_speech):
+    """
+    Проигрывание речи ответов голосового ассистента (без сохранения аудио)
+    :param text_to_speech: текст, который нужно преобразовать в речь
+    """
+    ttsEngine.say(str(text_to_speech))
+    ttsEngine.runAndWait()
+
+
+def record_and_recognize_audio():
     """
     Запись и распознавание аудио
     """
@@ -23,23 +57,94 @@ def record_and_recognize_audio(*args: tuple):
         recognizer.adjust_for_ambient_noise(microphone, duration=2)
 
         try:
-            print("Listening...")
+            print("Слушаю...")
             audio = recognizer.listen(microphone, 5, 5)
 
+            with open("microphone-results.wav", "wb") as file:
+                file.write(audio.get_wav_data())
+
         except speech_recognition.WaitTimeoutError:
-            print("Can you check if your microphone is on, please?")
+            print("Можете ли вы проверить работает ваши микрофон, пожалуйста?")
             return
 
-        # использование online-распознавания через Google 
+        # использование online-распознавания через Google
         try:
-            print("Started recognition...")
+            print("Начал узнавать...")
             recognized_data = recognizer.recognize_google(audio, language="ru").lower()
 
         except speech_recognition.UnknownValueError:
             pass
 
-        # в случае проблем с доступом в Интернет происходит выброс ошибки
+        # в случае проблем с доступом в Интернет происходит попытка 
+        # использовать offline-распознавание через Vosk
         except speech_recognition.RequestError:
-            print("Check your Internet Connection, please")
+            print("Попытка использовать офлайн-распознавание...")
+            recognized_data = use_offline_recognition()
 
         return recognized_data
+
+
+def use_offline_recognition():
+    """
+    Переключение на оффлайн-распознавание речи
+    :return: распознанная фраза
+    """
+    recognized_data = ""
+    try:
+        # проверка наличия модели на нужном языке в каталоге приложения
+        if not os.path.exists("models/vosk-model-small-ru-0.4"):
+            print("Пожалуйста, скачайте модель с:\n"
+                  "https://alphacephei.com/vosk/models и распаковать как 'модель' в текущую папку.")
+            exit(1)
+
+        # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
+        wave_audio_file = wave.open("microphone-results.wav", "rb")
+        model = Model("models/vosk-model-small-ru-0.4")
+        offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+
+        data = wave_audio_file.readframes(wave_audio_file.getnframes())
+        if len(data) > 0:
+            if offline_recognizer.AcceptWaveform(data):
+                recognized_data = offline_recognizer.Result()
+
+                # получение данных распознанного текста из JSON-строки
+                # (чтобы можно было выдать по ней ответ)
+                recognized_data = json.loads(recognized_data)
+                recognized_data = recognized_data["text"]
+    except:
+        print("К сожалению, голосовой сервис недоступен. Попробуйте позже")
+
+    return recognized_data
+
+
+if __name__ == "__main__":
+
+    # инициализация инструментов распознавания и ввода речи
+    recognizer = speech_recognition.Recognizer()
+    microphone = speech_recognition.Microphone()
+
+    # инициализация инструмента синтеза речи
+    ttsEngine = pyttsx3.init()
+
+    # настройка данных голосового помощника
+    assistant = VoiceAssistant()
+    assistant.name = "Jean"
+    assistant.sex = "female"
+    assistant.speech_language = "ru"
+
+    # установка голоса по умолчанию
+    setup_assistant_voice()
+
+    while True:
+        # старт записи речи с последующим выводом распознанной речи
+        # и удалением записанного в микрофон аудио
+        voice_input = record_and_recognize_audio()
+        os.remove("microphone-results.wav")
+        print(voice_input)
+
+        # отделение комманд от дополнительной информации (аргументов)
+        voice_input = voice_input.split(" ")
+        command = voice_input[0]
+
+        if command == "привет":
+            play_voice_assistant_speech("Здравствуйте")
