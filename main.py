3de2015601426eb7938b7d0ae7a4eebfb5b44658
White-prince <tiltedfear@gmail.com>
2022-11-27 22:41:18 +0300

    recode

--- main.py
+++ main.py
@@ -1,141 +1,16 @@
-from vosk import Model, KaldiRecognizer
-from pyowm import OWM
-from dotenv import load_dotenv
-import speech_recognition
-import pyttsx3
-import wikipediaapi
-import random
-import webbrowser
-import traceback
-import json
-import wave
-import os
-
-
-class Translate:
-    """
-    Получение вшитого в приложение перевода строк для создания мультиязычного ассистента
-    """
-    with open("Translate.json", "r", encoding="UTF-8") as file:
-        translations = json.load(file)
-
-    def get(self, text: str):
-        """
-        Получение перевода строки из файла на нужный язык
-        """
-        if text in self.translations:
-            return self.translations[text][assistant.speech_language]
-        else:
-            # в случае отсутствия перевода происходит вывод сообщения
-            print("Не переведенная фраза: {}".format(text))
-            return text
-
-
-class OwnerPerson:
-    """
-    Информация о владельце
-    """
-    name = "White-prince"
-    home_city = "Moscow"
-    native_language = "en"
-    target_language = "ru"
-
-
-class VoiceAssistant:
-    """
-    Настройки голосового ассистента
-    """
-    name = "Jeanne"
-    sex = "17"
-    speech_language = "ru"
-    recognition_language = "en"
+import speech_recognition  # распознание голоса
+from vosk import Model, KaldiRecognizer  # offline mode for Vosk
+import os  # работа с системой
+import wave  # запись
+import json  # js файлы
+import pyttsx3  # синтез речи
 
 
 def setup_assistant_voice():
     """
-    Установка голоса по умолчанию
+     Установка голоса по умолчанию
     """
     voices = ttsEngine.getProperty("voices")
-
-    if assistant.speech_language == "en":
-        assistant.recognition_language = "en-US"
-        if assistant.sex == "female":
-            ttsEngine.setProperty("voice", voices[1].id)
-        else:
-            ttsEngine.setProperty("voice", voices[2].id)
-    else:
-        assistant.recognition_language = "ru-RU"
-        ttsEngine.setProperty("voice", voices[0].id)
-
-
-def record_and_recognize_audio(*args):
-    """
-    Запись и распознавание аудио
-    """
-    with microphone:
-        recognized_data = ""
-
-        # запоминание шумов окружения для последующей очистки звука от них
-        recognizer.adjust_for_ambient_noise(microphone, duration=2)
-
-        try:
-            print("Listening...")
-            audio = recognizer.listen(microphone, 5, 5)
-
-            with open("microphone-results.wav", "wb") as file:
-                file.write(audio.get_wav_data())
-
-        except speech_recognition.WaitTimeoutError:
-            play_voice_assistant_speech(translator.get("Can you check if your microphone is on, please?"))
-            traceback.print_exc()
-            return
-
-        # использование online-распознавания через Google
-        try:
-            print("Started recognition...")
-            recognized_data = recognizer.recognize_google(audio, language=assistant.recognition_language).lower()
-
-        except speech_recognition.UnknownValueError:
-            pass  # play_voice_assistant_speech("What did you say again?")
-
-        # в случае проблем с доступом в Интернет происходит попытка использовать offline-распознавание через Vosk
-        except speech_recognition.RequestError:
-            print("Trying to use offline recognition...")
-            recognized_data = use_offline_recognition()
-
-        return recognized_data
-
-
-def use_offline_recognition():
-    """
-    Переключение на оффлайн-распознавание речи
-    """
-    recognized_data = ""
-    try:
-        # проверка наличия модели на нужном языке в каталоге приложения
-        if not os.path.exists("models/vosk-model-small-" + assistant.speech_language + "-0.4"):
-            print("Please download the model from:\n" +
-                  "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
-            exit(1)
-
-        # анализ записанного в микрофон аудио
-        wave_audio_file = wave.open("microphone-results.wav", "rb")
-        model = Model("models/vosk-model-small-" + assistant.speech_language + "-0.4")
-        offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
-
-        data = wave_audio_file.readframes(wave_audio_file.getnframes())
-        if len(data) > 0:
-            if offline_recognizer.AcceptWaveform(data):
-                recognized_data = offline_recognizer.Result()
-
-                # получение данных распознанного текста из JSON-строки
-                recognized_data = json.loads(recognized_data)
-                recognized_data = recognized_data["text"]
-    except "err":
-        traceback.print_exc()
-        print("Sorry, speech service is unavailable. Try again later")
-
-    return recognized_data
 
 
 def play_voice_assistant_speech(text_to_speech):
@@ -146,208 +21,84 @@ def play_voice_assistant_speech(text_to_speech):
     ttsEngine.runAndWait()
 
 
-def play_greetings(*args):
+def record_and_recognize_voice(*args: tuple):
     """
-    Проигрывание случайной приветственной речи
+    Функция записи и распознавания сказанного
     """
-    greetings = [
-        translator.get("Hello, {}! How can I help you today?").format(person.name),
-        translator.get("Good day to you {}! How can I help you today?").format(person.name)
-    ]
-    play_voice_assistant_speech(greetings[random.randint(0, len(greetings) - 1)])
+    with microphone:
+        recognized_data = ""
 
+        #  регулирование внешних шумов
+        recognizer.adjust_for_ambient_noise(microphone, duration=2)
+        try:
+            print('Говорите')
+            audio = recognizer.listen(microphone, 5, 5)
+            with open("microphone-results.wav", "wb") as file:
+                file.write(audio.get_wav_data())
+        except speech_recognition.WaitTimeoutError:
+            print('Я не могу распознать что вы говорите, проверьте свой микрофон!')
+            return
 
-def play_farewell_and_quit(*args):
-    """
-    Проигрывание прощательной речи и выход
-    """
-    farewells = [
-        translator.get("Goodbye, {}! Have a nice day!").format(person.name),
-        translator.get("See you soon, {}!").format(person.name)
-    ]
-    play_voice_assistant_speech(farewells[random.randint(0, len(farewells) - 1)])
-    ttsEngine.stop()
-    quit()
-
-
-def search_for_term_on_google(*args):
-    """
-    Поиск в Google с автоматическим открытием ссылок
-    """
-    if not args[0]:
-        return
-    search_term = " ".join(args[0])
-
-    # открытие ссылки на поисковик в браузере
-    url = "https://google.com/search?q=" + search_term
-    webbrowser.get().open(url)
-
-    play_voice_assistant_speech(translator.get("Here is what I found for {} on google").format(search_term))
-
-
-def search_for_video_on_youtube(*args: tuple):
-    """
-    Поиск видео на YouTube с автоматическим открытием ссылки на список результатов
-    """
-    if not args[0]:
-        return
-    search_term = " ".join(args[0])
-    url = "https://www.youtube.com/results?search_query=" + search_term
-    webbrowser.get().open(url)
-    play_voice_assistant_speech(translator.get("Here is what I found for {} on youtube").format(search_term))
-
-
-def search_for_definition_on_wikipedia(*args: tuple):
-    """
-    Поиск в Wikipedia определения с последующим озвучиванием результатов и открытием ссылок
-    """
-    if not args[0]:
-        return
-
-    search_term = " ".join(args[0])
-
-    # установка языка
-    wiki = wikipediaapi.Wikipedia(assistant.speech_language)
-
-    # поиск страницы по запросу, чтение summary, открытие ссылки на страницу для получения подробной информации
-    wiki_page = wiki.page(search_term)
+    #  online google распознавание
     try:
-        if wiki_page.exists():
-            play_voice_assistant_speech(translator.get("Here is what I found for {} on Wikipedia").format(search_term))
-            webbrowser.get().open(wiki_page.fullurl)
+        print('Запись начата')
+        recognized_data = recognizer.recognize_google(audio, language='ru').lower()
+    except speech_recognition.UnknownValueError:
+        pass
 
-            # чтение ассистентом первых двух предложений summary со страницы Wikipedia
-            play_voice_assistant_speech(wiki_page.summary.split(".")[:2])
-        else:
-            # открытие ссылки на поисковик в браузере в случае, если на Wikipedia не удалось найти ничего по запросу
-            play_voice_assistant_speech(translator.get(
-                "Can't find {} on Wikipedia. But here is what I found on google").format(search_term))
-            url = "https://google.com/search?q=" + search_term
-            webbrowser.get().open(url)
+    #  offline mode - Vosk
+    except speech_recognition.RequestError:
+        print("Trying to use offline recognition...")
+        recognized_data = offline_recognition()
 
-    except "":
-        play_voice_assistant_speech(translator.get("Oops"))
-        traceback.print_exc()
-        return
+    return recognized_data
 
 
-def get_weather_forecast(*args):
+def offline_recognition():
     """
-    Получение и озвучивание прогнза погоды
+    Функция offline записи и распознавания сказанного
     """
-    if args[0]:
-        city_name = args[0][0]
-    else:
-        city_name = person.home_city
+    recognized_data = ""
 
     try:
-        # использование ключа
-        weather_api_key = os.getenv("WEATHER_API_KEY")
-        open_weather_map = OWM(weather_api_key)
+        if not os.path.exists("model/vosk-model-small-ru-0.22"):
+            # проверка наличия модели на нужном языке
+            print('У вас нет модуля для данной функции!')
+            print('Устаеновите его тут:')
+            print('https://alphacephei.com/vosk/models и распакуйте в папку с программой')
+            exit(1)
 
-        # запрос данных о текущем состоянии погоды
-        weather_manager = open_weather_map.weather_manager()
-        observation = weather_manager.weather_at_place(city_name)
-        weather = observation.weather
+        wave_audio_file = wave.open("microphone-result.wav", 'rb')
+        model = Model("model/vosk-model-small-ru-0.22")
+        offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+        date = wave_audio_file.readframes(wave_audio_file.getnframes())
+        if len(date) > 0:
+            if offline_recognizer.AcceptWaveform(date):
+                recognized_data = offline_recognizer.Result()
+    except:
+        print('К сожалению, распознание сейчас не возможно')
 
-    # поскольку все ошибки предсказать сложно, то будет произведен отлов с последующим выводом без остановки программы
-    except "":
-        play_voice_assistant_speech(translator.get("Oops"))
-        traceback.print_exc()
-        return
-
-    # разбивание данных на части для удобства работы с ними
-    status = weather.detailed_status
-    temperature = weather.temperature('celsius')["temp"]
-    wind_speed = weather.wind()["speed"]
-    pressure = int(weather.pressure["press"] / 1.333)  # переведено из гПА в мм рт.ст.
-
-    # вывод логов
-    print("Weather in " + city_name +
-          ":\n * Status: "
-          + status +
-          "\n * Wind speed (m/sec): " +
-          str(wind_speed) +
-          "\n * Temperature (Celsius): " +
-          str(temperature) +
-          "\n * Pressure (mm Hg): " +
-          str(pressure))
-
-    # озвучивание текущего состояния погоды ассистентом (здесь для мультиязычности требуется дополнительная работа)
-    play_voice_assistant_speech(translator.get("It is {0} in {1}").format(status, city_name))
-    play_voice_assistant_speech(translator.get("The temperature is {} degrees Celsius").format(str(temperature)))
-    play_voice_assistant_speech(translator.get("The wind speed is {} meters per second").format(str(wind_speed)))
-    play_voice_assistant_speech(translator.get("The pressure is {} mm Hg").format(str(pressure)))
+    return recognized_data
 
 
-def change_language(*args):
-    """
-    Изменение языка голосового ассистента (языка распознавания речи)
-    """
-    assistant.speech_language = "ru" if assistant.speech_language == "en" else "en"
-    setup_assistant_voice()
-    print("Language switched to " + assistant.speech_language)
+if __name__ == '__main__':
 
-
-def execute_command_with_name(command_name: str, *args: list):
-    """
-    Выполнение заданной пользователем команды и аргументами
-    """
-    for key in commands.keys():
-        if command_name in key:
-            commands[key](*args)
-        else:
-            pass
-
-
-commands = {
-    ("hello", "hey", "hi", "morning", "привет"): play_greetings,
-    ("bye", "goodbye", "quit", "exit", "stop", "пока"): play_farewell_and_quit,
-    ("search", "google", "find", "найди"): search_for_term_on_google,
-    ("video", "youtube", "watch", "видео"): search_for_video_on_youtube,
-    ("wikipedia", "definition", "about", "определение", "википедия"): search_for_definition_on_wikipedia,
-    ("language", "язык"): change_language,
-    ("weather", "forecast", "погода", "прогноз"): get_weather_forecast
-}
-
-if __name__ == "__main__":
-
-    # инициализация инструментов распознавания и ввода речи
+    #  распознавание и ввод речи
     recognizer = speech_recognition.Recognizer()
     microphone = speech_recognition.Microphone()
-
-    # инициализация инструмента синтеза речи
+    # синтез речи
     ttsEngine = pyttsx3.init()
-
-    # настройка данных пользователя
-    person = OwnerPerson()
-    person.name = "White-prince"
-    person.home_city = "Moscow"
-    person.native_language = "ru"
-    person.target_language = "en"
-
-    # настройка данных голосового помощника
-    assistant = VoiceAssistant()
-    assistant.name = "Jeanne"
-    assistant.sex = "female"
-    assistant.speech_language = "ru"
-
     # установка голоса по умолчанию
     setup_assistant_voice()
 
-    # добавление возможностей перевода фраз
-    translator = Translate()
-
-    # загрузка информации из .env-файла
-    load_dotenv()
-
     while True:
-        # старт записи речи с последующим выводом распознанной речи и удалением записанного в микрофон аудио
-        voice_input = record_and_recognize_audio()
+        #  старт записи речи с последующим записи речи
+        voice_input = record_and_recognize_voice()
         os.remove("microphone-results.wav")
+        print(voice_input)
 
-        # отделение комманд от дополнительной информации
         voice_input = voice_input.split(" ")
         command = voice_input[0]
-        command_options = [str(input_part) for input_part in voice_input[1:len(voice_input)]]
-        execute_command_with_name(command, command_options)
+
+        if command == "привет":
+            play_voice_assistant_speech("Здравствуй")
